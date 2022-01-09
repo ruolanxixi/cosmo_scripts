6aa5b59f4817e8fa7cb9c94a18ecc03b9b2abe9e
Ruolan <36844264+ruolanxixi@users.noreply.github.com>
2022-01-09 21:19:01 +0100

    Update cosmoFunctions.sh

--- cosmoFunctions.sh
+++ cosmoFunctions.sh
@@ -44,7 +44,7 @@ outPath=/project/pr94/rxiang/analysis/EAS$2_$3
 if test -f "$outPath/$4_$1_mergetime.nc"; then
     rm $outPath/$4_$1_mergetime.nc
 fi
-
+echo "1"
 cdo mergetime $inPath/$1.nc $outPath/$4_$1_mergetime.nc
 echo "files merged"
 
