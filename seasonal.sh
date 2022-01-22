f1cc1d14843dfc1a05660a82da32f7bf54c8184b
Ruolan <36844264+ruolanxixi@users.noreply.github.com>
2022-01-22 19:47:37 +0100

    Update seasonal.sh

--- seasonal.sh
+++ seasonal.sh
@@ -5,12 +5,12 @@ source cosmoFunctions.sh
 # 
 # TOT_PREC U V --- summer precipitation and summer winds at 850 hPa [kg m-2 >> mm day-1]
 # W TQV --- summer vertical velocity at 500 hPa and summer TQV (total precipitable water) [kg m-2 >> mm day-1]
-# T --- summer temperature at 500 hPa [K >> oC]
+# T FI --- summer temperature and geopotential at 500 hPa [K >> oC]
 # SOHR_RAD THHR_RAD DT_CON DT_SSO --- diabatic heating [K s-1 >> K day-1]
 # Omega --- lagrangian tendency of air pressure [Pa s-1 >> hPa day-1]
 # SLP --- winter monsoon winds at 850 hPa and see level pressure [Pa >> hPa]
 
-declare -a var=("TOT_PREC" "U" "V" "W" "TQV" "QV" "T_2M" "T" "SOHR_RAD" "THHR_RAD" "DT_CON" "DT_SSO" "Omega" "SLP") 
+declare -a var=("TOT_PREC" "U" "V" "W" "TQV" "QV" "T_2M" "T" "FI" "SOHR_RAD" "THHR_RAD" "DT_CON" "DT_SSO" "Omega" "SLP") 
 declare -a sim=("ctrl" "topo1")
 resolution=11
 year=01
