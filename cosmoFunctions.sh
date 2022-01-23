59cc02d5a1d78c9cc5330de3c0f1169dac068e90
Ruolan <36844264+ruolanxixi@users.noreply.github.com>
2022-01-23 21:27:46 +0100

    Update cosmoFunctions.sh

--- cosmoFunctions.sh
+++ cosmoFunctions.sh
@@ -53,8 +53,8 @@ outPath=/project/pr94/rxiang/analysis/EAS$2_$3/$1
 [ ! -d "$outPath" ] && mkdir -p "$outPath"
 
 # if file exits, remove it
-if test -f "$outPath/$4_$1_mergetime.nc"; then
-    rm $outPath/$4_$1_mergetime.nc
+if test -f "$outPath/$4_$1.nc"; then
+    rm $outPath/$4_$1.nc
 fi
 
 cdo mergetime $inPath/$1.nc $outPath/$4_$1.nc
