685d21102ff0ecff748ff121bf29b020a7112d4b
xxKaktus <kaktusxrl@gmail.com>
2022-01-25 10:12:06 +0100

    .

--- plot_prec_wind.py
+++ plot_prec_wind.py
@@ -10,12 +10,14 @@ import numpy as np
 import matplotlib.colors as colors
 from copy import copy
 import matplotlib.gridspec as gridspec
+import pylab
 
 # -------------------------------------------------------------------------------
 # import data
 #
 path1 = "/Users/kaktus/Documents/ETH/BECCY/myscripts/data/ctrl/"
 path2 = "/Users/kaktus/Documents/ETH/BECCY/myscripts/data/topo1/"
+
 file1 = '01_TOT_PREC_JJA_cut.nc'
 file2 = '01_U_85000_JJA_cut.nc'
 file3 = '01_V_85000_JJA_cut.nc'
@@ -30,7 +32,7 @@ ds = xr.open_dataset(path2 + file1)
 prec_topo1 = ds["TOT_PREC"].values[0, :, :]
 ds.close()
 
-prec_diff = prec_ctrl - prec_topo1
+prec_diff = (prec_ctrl+0.0000000000001 - prec_topo1)/(prec_ctrl+0.0000000000001)*100
 
 ds = xr.open_dataset(path1 + file2)
 u_ctrl = ds["U"].values[0, 0, :, :]
@@ -54,11 +56,16 @@ v_diff = v_ctrl - v_topo1
 # -------------------------------------------------------------------------------
 # plot
 #
+# pylab.rcParams['xtick.major.pad']='8'
+# pylab.rcParams['ytick.major.pad']='8'
+
 ar = 1.0  # initial aspect ratio for first trial
 wi = 15  # width in inches
 hi = wi * ar  # height in inches
+ncol = 3
+nrow = 1
 
-gs = gridspec.GridSpec(1, 3)
+gs = gridspec.GridSpec(nrow, ncol)
 fig = plt.figure(figsize=(wi, hi))
 
 projection = ccrs.LambertConformal(central_longitude=115, central_latitude=28.5)
@@ -68,11 +75,11 @@ axs1 = plt.subplot(gs[1], projection=projection)
 axs2 = plt.subplot(gs[2], projection=projection)
 
 cs0 = axs0.contourf(lon, lat, prec_ctrl, transform=ccrs.PlateCarree(), levels=np.linspace(0, 16, 11), cmap='YlGnBu', vmin=0, vmax=20, extend='max')
-# axs0.contour(lon, lat, prec_ctrl, cs0.levels, colors='k', linewidths=.3)
+# axs0.contour(lon, lat, prec_ctrl, cs0.levels, colors='k', linewidths=.1)
 cs1 = axs1.contourf(lon, lat, prec_topo1, transform=ccrs.PlateCarree(), levels=np.linspace(0, 16, 11), cmap='YlGnBu', vmin=0, vmax=20, extend='max')
-# axs1.contour(lon, lat, prec_topo1, cs1.levels, colors='k', linewidths=.3)
-cs2 = axs2.contourf(lon, lat, prec_diff, transform=ccrs.PlateCarree(), levels=np.linspace(-5, 5, 11), cmap='RdYlBu', vmin=-5, vmax=5)
-# axs2.contour(lon, lat, prec_diff, cs2.levels, colors='k', linewidths=.3)
+# axs1.contour(lon, lat, prec_topo1, cs1.levels, colors='k', linewidths=.1)
+cs2 = axs2.contourf(lon, lat, prec_diff, transform=ccrs.PlateCarree(), levels=np.linspace(-50, 50, 21), cmap='RdYlBu', vmin=-50, vmax=50)
+# axs2.contour(lon, lat, prec_diff, cs2.levels, colors='k', linewidths=.1)
 
 # add wind component
 q0 = axs0.quiver(lon[::15,::15], lat[::15,::15], u_ctrl[::15,::15], v_ctrl[::15,::15], transform=projection, color='red', scale=130)
@@ -86,11 +93,11 @@ axs0.set_title("Control", fontweight='bold', pad=10)
 axs1.set_title("Reduced topography 1", fontweight='bold', pad=10)
 axs2.set_title("Difference", fontweight='bold', pad=10)
 
-axs0.text(0.05, 0.95, 'a', ha='center', va='center', transform=axs0.transAxes,
+axs0.text(0.03, 0.96, 'a', ha='center', va='center', transform=axs0.transAxes,
           fontsize=12, family='sans-serif')
-axs1.text(0.05, 0.95, 'b', ha='center', va='center', transform=axs1.transAxes,
+axs1.text(0.03, 0.96, 'b', ha='center', va='center', transform=axs1.transAxes,
           fontsize=12, family='sans-serif')
-axs2.text(0.05, 0.95, 'c', ha='center', va='center', transform=axs2.transAxes,
+axs2.text(0.03, 0.96, 'c', ha='center', va='center', transform=axs2.transAxes,
           fontsize=12, family='sans-serif')
 
 axs = [axs0, axs1, axs2]
@@ -116,20 +123,20 @@ gl2.right_labels = False
 gl2.top_labels = False
 
 plt.tight_layout()
-plt.subplots_adjust(left=0.03, bottom=None, right=None, top=None, wspace=0.1, hspace=0)
+plt.subplots_adjust(left=0.03, bottom=None, right=None, top=0.90, wspace=0.1, hspace=0)
 
-cb1 = fig.colorbar(cs1, orientation='horizontal', shrink=0.9, aspect=70, ax=axs[0:2], pad=0.03)
-cb2 = fig.colorbar(cs2, orientation='horizontal', shrink=0.9, aspect=30, ax=axs[2], pad=0.03)
+cb1 = fig.colorbar(cs1, orientation='horizontal', shrink=0.9, aspect=70, ax=axs[0:2], pad=0.07)
+cb2 = fig.colorbar(cs2, orientation='horizontal', shrink=0.9, aspect=30, ax=axs[2], pad=0.07)
 
-cb1.set_label('[mm/day]')
+cb1.set_label('mm/day')
 # cb1.set_ticks([0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000])
-cb2.set_label('[mm/day]')
+cb2.set_label('%')
 
 xmin, xmax = axs[0].get_xbound()
 ymin, ymax = axs[0].get_ybound()
-y2x_ratio = (ymax - ymin) / (xmax - xmin)
+y2x_ratio = (ymax - ymin) / (xmax - xmin) * nrow / ncol * 1.4
 
 fig.set_figheight(wi * y2x_ratio)
 plt.show()
 
-#fig.savefig('figure_prec_wind.png', dpi=300)
+fig.savefig('figure_prec_wind.png', dpi=300)
