c881b8c7c7982ba1dcae39cb23615561c96eebb6
xxKaktus <kaktusxrl@gmail.com>
2022-06-29 21:24:43 +0200

    .

---
+++ plot_shal/plot_asr.py
@@ -0,0 +1,135 @@
+# -------------------------------------------------------------------------------
+# modules
+#
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+from plotcosmomap import plotcosmo04, pole04, colorbar, custom_div_cmap
+import matplotlib.colors as colors
+from numpy import inf
+import cmcrameri.cm as cmc
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
+
+# -------------------------------------------------------------------------------
+# import data
+#
+mdnames = ['ctrl', 'SHAL']
+mdvname1 = 'ASOB_T'  # edit here
+mdvname2 = 'ASOD_T'  # edit here
+year = '200106'
+mdpath = "/project/pr133/rxiang/data/cosmo/"
+erapath = "/project/pr133/rxiang/data/era5/ot/remap/"
+cerespath = "/project/pr133/rxiang/data/obs/rd/CERES/remap/"
+
+# -------------------------------------------------------------------------------
+# read model data
+#
+mddata1, mddata2 = [], []
+for mds in range(len(mdnames)):
+    md = mdnames[mds]
+    filename = f'{mdvname1}.nc'
+    data = xr.open_dataset(f'{mdpath}EAS04_{md}/{year}/{filename}')[mdvname1].values[0, :, :]
+    mddata1.append(data)
+    filename = f'{mdvname2}.nc'
+    data = xr.open_dataset(f'{mdpath}EAS04_{md}/{year}/{filename}')[mdvname2].values[0, :, :]
+    mddata2.append(data)
+
+# -------------------------------------------------------------------------------
+# read era5 data
+#
+filename = f'era5.mo.{year}.remap.04.nc'
+data1 = xr.open_dataset(f'{erapath}{filename}')['mtnswrf'].values[0, :, :]
+data2 = xr.open_dataset(f'{erapath}{filename}')['mtdwswrf'].values[0, :, :]
+eradata = data2 - data1
+
+# -------------------------------------------------------------------------------
+# read observation data
+#
+
+filename = f'CERES.{year}.1.remap.04.nc'
+otdata = xr.open_dataset(f'{cerespath}{filename}')['toa_sw_all_mon'].values[0, :, :]
+
+# -------------------------------------------------------------------------------
+# compute difference
+#
+diffdata = []
+for i in range(len(mdnames)):
+    data = mddata2[i] - mddata1[i] - eradata
+    diffdata.append(data)
+for i in range(len(mdnames)):
+    data = mddata2[i] - mddata1[i] - otdata
+    diffdata.append(data)
+
+# -------------------------------------------------------------------------------
+# plot
+#
+[pole_lat, pole_lon, lat, lon, rlat, rlon, rot_pole_crs] = pole04()
+#
+ar = 1.0  # initial aspect ratio for first trial
+hi = 14  # height in inches
+wi = hi / ar  # width in inches
+# fig = plt.figure(figsize=(wi, hi))
+#
+ncol = 3  # edit here
+nrow = 2
+# gs = gridspec.GridSpec(nrow, ncol, figure=fig)
+fig, axs = plt.subplots(nrow, ncol, figsize=(wi, hi), subplot_kw={'projection': rot_pole_crs})
+cs = np.empty(shape=(nrow, ncol), dtype='object')
+# -------------------------
+# panel plot
+for i in range(nrow):
+    cs[i % 2, i // 2] = axs[i % 2, i // 2].pcolormesh(rlon, rlat, mddata2[i] - mddata1[i], cmap=cmc.roma_r, shading="auto")
+    ax = plotcosmo04(axs[i % 2, i // 2])
+
+cmap = custom_div_cmap(27, cmc.vik)
+levels = MaxNLocator(nbins=26).tick_values(-40, 100)
+norm = colors.TwoSlopeNorm(vmin=-40., vcenter=0., vmax=100.)
+cs[1, 0] = axs[1, 0].pcolormesh(rlon, rlat, mddata2[0] - mddata1[0] - mddata2[1] + mddata1[1], cmap=cmap, norm=norm, shading="auto")
+for i in np.arange(nrow, ncol * nrow, 1):
+    cs[i % 2, i // 2] = axs[i % 2, i // 2].pcolormesh(rlon, rlat, diffdata[i-2], cmap=cmap, norm=norm, shading="auto")
+    ax = plotcosmo04(axs[i % 2, i // 2])
+
+# -------------------------
+# add title
+axs[0, 0].set_title("COSMO", fontweight='bold', pad=10, fontsize=14)
+axs[0, 1].set_title("COSMO-ERA5", fontweight='bold', pad=10, fontsize=14)
+axs[0, 2].set_title("COSMO-CERES", fontweight='bold', pad=10, fontsize=14)
+
+# -------------------------
+# add label
+axs[0, 0].text(-0.14, 0.55, 'Explicit', ha='center', va='center', rotation='vertical',
+               transform=axs[0, 0].transAxes, fontsize=14, fontweight='bold')
+axs[1, 0].text(-0.14, 0.55, 'Explicit - Shallow', ha='center', va='center', rotation='vertical',
+               transform=axs[1, 0].transAxes, fontsize=14, fontweight='bold')
+# -------------------------
+# adjust figure
+# plt.subplots_adjust(left=None, bottom=None, right=None, top=None)
+xmin, xmax = axs[0, 0].get_xbound()
+ymin, ymax = axs[0, 0].get_ybound()
+y2x_ratio = (ymax - ymin) / (xmax - xmin) * nrow / ncol * 1.05
+fig.set_figwidth(hi / y2x_ratio)
+plt.subplots_adjust(left=0.05, bottom=0.08, right=0.98, top=0.95, wspace=0.1, hspace=0.09)
+
+# -------------------------
+# add colorbar
+wspace=0.03
+cax = colorbar(fig, axs[1, 0], 1, wspace)  # edit here
+cb1 = fig.colorbar(cs[0, 0], cax=cax, orientation='horizontal', extend='both')
+cb1.set_label('$W/m^2$', fontsize=12)
+cb1.ax.tick_params(labelsize=12)
+cax = colorbar(fig, axs[1, 1], 2, wspace)  # edit here
+cb2 = fig.colorbar(cs[1, 1], cax=cax, orientation='horizontal', ticks=np.append(np.linspace(-40, 0, 5), np.linspace(20, 100, 5)), extend='both')
+cb2.set_label('$W/m^2$', fontsize=12)
+cb2.ax.tick_params(labelsize=12)
+# cax = colorbar(fig, axs[3, 1], 1)
+# cb2 = fig.colorbar(cs[3, 1], cax=cax, orientation='horizontal', extend='both')
+# # # cb1.set_ticks([0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000])
+# cb2.set_label('%')
+
+plt.show()
+# -------------------------
+# save figure
+plotpath = "/project/pr133/rxiang/figure/shal/"
+fig.savefig(plotpath + 'asr.png', dpi=300)
+plt.close(fig)
--- plotcosmomap.py
+++ plotcosmomap.py
@@ -131,6 +131,75 @@ def plotcosmo(ax):
     return ax
 
 
+
+def plotcosmo04(ax):
+    """
+    A function to draw the background map for plotting CCLM output.
+            The output-map will be ploted on a rotated pole grid.
+
+            Args:
+                    infile: an xarray data array or a structure that contains lat and lon data
+
+                    ax: axes
+
+                    plabels (optional): label definition for parallels
+
+                    mlabels (optional): labels for meridians
+
+                    additional optional arguments with default are resolution of the coastlines, linewidth for meridians and the fontsize of the labels. additional text and line **kwargs can also be passed
+
+            Returns:
+                    m: the basemap map projection (rotated pole) used in CCLM
+
+            Example usage:
+                    from plotcosmomap import plotcosmomap
+
+                    mydata=xr.open_dataset('mypath')
+
+                    m, xi, yi = plotcosmomap(mydata); m.pcolormesh(xi, yi, mydata, cmap='plasma')
+    """
+
+    ax.set_extent([88, 112.5, 17, 39], crs=ccrs.PlateCarree())  # for extended 12km domain
+    # ax.add_feature(cfeature.LAND)
+    ax.stock_img()
+    # ax.add_feature(cfeature.OCEAN, zorder=100)
+    ax.add_feature(cfeature.LAND, edgecolor='k')
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.BORDERS, linestyle=':')
+    ax.add_feature(cfeature.LAKES, alpha=0.5)
+    ax.add_feature(cfeature.RIVERS)
+
+    # add_gridline_labels(ax, labels_set=[10, 20, 30, 40, 50, 60, 70], side='left')
+    # add_gridline_labels(ax, labels_set=[0, 10, 20, 30, 40, 50, 60], side='right')
+    # add_gridline_labels(ax, labels_set=[60, 100, 140, 180], side='top')
+    # add_gridline_labels(ax, labels_set=[80, 100, 120, 140, 160], side='bottom')
+    gl = ax.gridlines(draw_labels=False, dms=True, x_inline=False, y_inline=False, linewidth=1,
+                      color='grey', alpha=0.5, linestyle='--')
+    gl.xlocator = mticker.FixedLocator([90, 100, 110, 120])
+    gl.ylocator = mticker.FixedLocator([15, 20, 25, 30, 35, 40])
+
+    # add ticks manually
+    ax.text(-0.05, 0.88, '35°N', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+    ax.text(-0.05, 0.67, '30°N', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+    ax.text(-0.05, 0.46, '25°N', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+    ax.text(-0.05, 0.25, '20°N', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+    ax.text(-0.05, 0.04, '15°N', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+
+    ax.text(0.04, -0.04, '90°E', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+    ax.text(0.46, -0.04, '100°E', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+    ax.text(0.86, -0.04, '110°E', ha='center', va='center', transform=ax.transAxes, fontsize=13)
+
+    # ax.set_xticks([80, 100, 120, 140, 160], crs=ccrs.PlateCarree())
+    # ax.set_yticks([0, 10, 20, 30, 40, 50, 60], crs=ccrs.PlateCarree())
+    # lon_formatter = LongitudeFormatter(number_format='.0f', degree_symbol='°')
+    # lat_formatter = LatitudeFormatter(number_format='.0f', degree_symbol='°')
+    # ax.xaxis.set_major_formatter(lon_formatter)
+    # ax.yaxis.set_major_formatter(lat_formatter)
+
+    return ax
+
+
+
 def pole():
     file = "/project/pr133/rxiang/data/cosmo/EAS11_ctrl/szn/T_2M/01_T_2M_DJF.nc"
     ds = xr.open_dataset(f'{file}')
@@ -145,17 +214,53 @@ def pole():
     return pole_lat, pole_lon, lat, lon, rlat, rlon, rot_pole_crs
 
 
-def colorbar(fig, ax, n):
+def pole04():
+    file = "/project/pr133/rxiang/data/cosmo/EAS04_ctrl/200106/T_2M.nc"
+    ds = xr.open_dataset(f'{file}')
+    pole_lat = ds["rotated_pole"].grid_north_pole_latitude
+    pole_lon = ds["rotated_pole"].grid_north_pole_longitude
+    lat = ds["lat"].values
+    lon = ds["lon"].values
+    rlat = ds["rlat"].values
+    rlon = ds["rlon"].values
+    rot_pole_crs = ccrs.RotatedPole(pole_latitude=pole_lat, pole_longitude=pole_lon)
+
+    return pole_lat, pole_lon, lat, lon, rlat, rlon, rot_pole_crs
+
+
+
+def colorbar(fig, ax, n, wspace):
     if n == 1:
         cax = fig.add_axes([ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width, 0.012])
     elif n == 2:
         cax = fig.add_axes(
-            [ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width * 2.145, 0.012])
+            [ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width * 2 + wspace, 0.012])
     elif n == 3:
         cax = fig.add_axes(
-            [ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width * 3.275, 0.012])
+            [ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width * 3 + wspace*2, 0.012])
     elif n == 4:
         cax = fig.add_axes(
-            [ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width * 4.4, 0.012])
+            [ax.get_position().x0, ax.get_position().y0 - 0.04, ax.get_position().width * 4 + wspace*3, 0.012])
 
     return cax
+
+
+
+def custom_div_cmap(numcolors, colormap):
+    """ Create a custom diverging colormap with three colors
+
+    Default is blue to white to red with 11 colors.  Colors can be specified
+    in any way understandable by matplotlib.colors.ColorConverter.to_rgb()
+    """
+
+    from matplotlib.colors import LinearSegmentedColormap
+
+    colors_red = colormap(np.linspace(0, 0.5, 20))
+    colors_blue = colormap(np.linspace(0.55, 1, 20))
+    colors_white = np.array([1, 1, 1, 1])
+    colors = np.vstack((colors_red, colors_white, colors_blue))
+
+    cmap = LinearSegmentedColormap.from_list(name=colormap, colors=colors, N=numcolors)
+
+    return cmap
+
